1f978ee81ced1b194e891df2b9a03ead3c7989c1
Gurprem Bhullar <bhullargurprem@gmail.com>
2020-07-18 09:57:48 -0400

    started working with some React

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const PORT = process.env.PORT || 8082;
+const mongoose = require("mongoose";)
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build")); 
+app.use(express.urlencoded)({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongdb://localhost/testingMongo",
+{
+  useNewUrlParser: true,
+  useFindAndModify: false,
 }
+)
+const db = require("./models");
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get("/test", ( req, res) => {
+  res.json("It's working");
+});
+
+app.post("/test", async (req, res) => {
+
+}
+app.get("/test", function(req, res) => {
+  console.log
 });
 
 app.listen(PORT, function() {
