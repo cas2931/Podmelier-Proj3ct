d20d197066dda95c6af74d55fea6199275630d4c
Silver Snail 57 <cdarkly1392@gmail.com>
2020-07-21 17:07:51 -0400

    !

---
+++ client/build/precache-manifest.de6b8a3cfa0f8222395d203e2899ff44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "510b236a260054c77c5f",
+    "url": "/static/css/main.4ce01376.chunk.css"
+  },
+  {
+    "revision": "510b236a260054c77c5f",
+    "url": "/static/js/main.4e47f7b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "562e6ea4c01c6a5d76e1",
+    "url": "/static/css/2.b3f36135.chunk.css"
+  },
+  {
+    "revision": "562e6ea4c01c6a5d76e1",
+    "url": "/static/js/2.a3da730e.chunk.js"
+  },
+  {
+    "revision": "4672b6ee882107209d3361bb62e71b70",
+    "url": "/index.html"
+  }
+];
